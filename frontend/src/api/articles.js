f10232d3150da3e181a621128d2351e4f277c1c3
sumit sangave <sumitsangave2631@gmail.com>
2026-01-01 01:07:34 +0530

    Preapred frontend for the Vercel deployment

--- frontend/src/api/articles.js
+++ frontend/src/api/articles.js
@@ -1,8 +1,13 @@
 import axios from "axios";
 
-const API = "http://localhost:5000/api/articles";
+const API_BASE = "https://beyondchats-assignment-ev5q.onrender.com/api";
 
 export const fetchArticles = async () => {
-  const res = await axios.get(API);
+  const res = await axios.get(`${API_BASE}/articles`);
+  return res.data;
+};
+
+export const triggerRewrite = async (id) => {
+  const res = await axios.post(`${API_BASE}/articles/${id}/rewrite`);
   return res.data;
 };
